22704f2c71b4d75c26b6461f95b76f81b50e5143
liveralmask <liveralmask.lisk@gmail.com>
2015-01-03 18:32:04 +0900

    rails new mondai -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mondai::Application.config.secret_key_base = '1f90e398d399b942a110135d7195497ec151405c96923ab84aa19fbb42d326cf20ae076f118a7b627f0cafaeac70c95e4b2eea8e80c31500a99c2b7d4a168874'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mondai::Application.config.session_store :cookie_store, key: '_mondai_session'
